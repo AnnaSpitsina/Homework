613553a903baae35e0beb4f010de5cb9ae1e0df8
Spitsina Anna <spitsina.madam@yansex.ru>
2022-03-30 22:35:36 +0300

    kompill

--- project/include/utils.h
+++ project/include/utils.h
@@ -1,9 +1,29 @@
-#define UTILS_H
 #ifndef UTILS_H
+#define UTILS_H
 
-#define filename "transaction.dat";
+#include <stdio.h>
 
-// This comment is very useful
-printf("This message is very useful too")
+#define filename "transaction.dat"
 
-#endif //UTILS_H
+
+struct masterRecord {
+  int Number;
+  char Name[20];
+  char Surname[20];
+  char addres[30];
+  char TelNumber[15];
+  double indebtedness;
+  double credit_limit;
+  double cash_payments;
+};
+typedef struct masterRecord Data;
+
+void masterWrite(FILE* ofPTR, Data Client);
+void transactionWrite(FILE* ofPTR, Data transfer);
+void FuncBlackRecord(FILE* ofPTR,
+                 FILE* ofPTR_2,
+                 FILE* blackrecord,
+                 Data client_data,
+                 Data transfer);
+
+#endif  // UTILS_H
